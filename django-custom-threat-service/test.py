6a57b8f988199af74d79a4c1f26e93f2bed2d2e0
Mark-Scherfling <mark.scherfling@ibm.com>
2019-07-24 15:22:27 -0400

    updates for newer version of django

---
+++ django-custom-threat-service/test.py
@@ -0,0 +1,4 @@
+# uwgsi test file from https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Hello World"] # python3
